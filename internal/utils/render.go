8dc48db6e2e8638c4920d067b066345149acd2d8
ferdie bergado <ferdiebergado@gmail.com>
2024-08-31 17:51:30 +0800

    Refactored to propagate error to the error handler

--- internal/utils/render.go
+++ internal/utils/render.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"bytes"
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -23,44 +22,32 @@ func logResult(templateName string, isHit bool) {
 	log.Printf("Template cache %s: %s", result, templateName)
 }
 
-func parseTemplate(templateName string) (*template.Template, error) {
-
-	const templatePathFormat string = "%s/%s"
+func parseTemplate(templateName string) *template.Template {
 
 	if cachedTemplate, ok := templateCache.Load(templateName); ok {
 		logResult(templateName, true)
-		return cachedTemplate.(*template.Template), nil
+		return cachedTemplate.(*template.Template)
 	}
 
 	logResult(templateName, false)
 
-	layoutPath := fmt.Sprintf(templatePathFormat, config.TemplatesDir, config.MasterTemplate)
-	templatePath := fmt.Sprintf(templatePathFormat, config.TemplatesDir, templateName)
+	layoutPath := config.TemplatesDir + "/" + config.MasterTemplate
+	templatePath := config.TemplatesDir + "/" + templateName
 
-	tmpl, err := template.ParseFiles(layoutPath, templatePath)
-
-	if err != nil {
-		return nil, err
-	}
+	tmpl := template.Must(template.ParseFiles(layoutPath, templatePath))
 
 	templateCache.Store(templateName, tmpl)
 
-	return tmpl, nil
+	return tmpl
 }
 
 func Render(w http.ResponseWriter, tmpl string, data interface{}) {
-	t, err := parseTemplate(tmpl)
-
-	if err != nil {
-		HandleHTTPError(w, "unable to parse files", err)
-		return
-	}
+	t := parseTemplate(tmpl)
 
 	var buf bytes.Buffer
 
 	if err := t.ExecuteTemplate(&buf, tmpl, data); err != nil {
-		HandleHTTPError(w, "unable to execute template", err)
-		return
+		panic(err)
 	}
 
 	buf.WriteTo(w)
