13170615a79bfbd035ce6d7efcc446d690d9a06c
ferdie bergado <ferdiebergado@gmail.com>
2024-09-02 17:54:59 +0800

    Included debug stack in error log

--- internal/middlewares/error.go
+++ internal/middlewares/error.go
@@ -1,22 +1,26 @@
 package middlewares
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"runtime/debug"
 
 	"github.com/ferdiebergado/htmx-go/internal/utils"
 )
 
 func ErrorHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 		defer func() {
 			if err := recover(); err != nil {
-				log.Printf("Recovered from panic: %v\n", err)
-				// http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				log.Println(fmt.Errorf("error: %v", err))
+				log.Println(string(debug.Stack()))
 				w.WriteHeader(http.StatusInternalServerError)
 				utils.Render(w, "error.html", nil)
 			}
 		}()
+
 		next.ServeHTTP(w, r)
 	})
 }
