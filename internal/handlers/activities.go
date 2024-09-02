1fd5ac02d4ce8c6c00ed44e5feb412a6a6a78a3c
ferdie bergado <ferdiebergado@gmail.com>
2024-09-02 18:03:00 +0800

    Refactored to use repository and implemented view activity feature

--- internal/handlers/activities.go
+++ internal/handlers/activities.go
@@ -1,92 +1,65 @@
 package handlers
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
-	"github.com/ferdiebergado/htmx-go/internal/db"
 	"github.com/ferdiebergado/htmx-go/internal/models"
+	"github.com/ferdiebergado/htmx-go/internal/services"
 	"github.com/ferdiebergado/htmx-go/internal/utils"
 )
 
 type ActivityHandler struct {
-	DB db.Database
-}
-
-func (h *ActivityHandler) CreateActivity(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-
-	if err != nil {
-		http.Error(w, "Error parsing form data", http.StatusBadRequest)
-		return
-	}
-
-	activityStatus, err := strconv.Atoi(r.FormValue("status"))
-
-	if err != nil {
-		http.Error(w, "Invalid age input", http.StatusBadRequest)
-		return
-	}
-
-	title := r.FormValue("title")
-	start := r.FormValue("start_date")
-	end := r.FormValue("end_date")
-	venue := r.FormValue("venue")
-	host := r.FormValue("host")
-	status := activityStatus
-	remarks := r.FormValue("remarks")
-
-	query := `INSERT INTO activities 
-	(title, start_date, end_date, venue, host, status,remarks) 
-	VALUES ($1,$2,$3,$4,$5,$6,$7)`
-
-	rows, err := h.DB.Query(r.Context(), query, title, start, end, venue, host, status, remarks)
-
-	if err != nil {
-		utils.HandleHTTPError(w, "Query failed", err)
-		return
-	}
-
-	defer rows.Close()
-
-	utils.Render(w, "activities.html", nil)
+	Repository models.ActivityRepository
+	Session    *services.SessionManager
 }
 
 func (h *ActivityHandler) ListActivities(w http.ResponseWriter, r *http.Request) {
 
-	sql := `SELECT id, title, start_date, end_date, venue, host, status, remarks FROM activities`
-
-	rows, err := h.DB.Query(r.Context(), sql)
-
-	if err != nil {
-		utils.HandleHTTPError(w, "query failed", err)
-		return
-	}
-
-	defer rows.Close()
-
 	var activities []interface{}
 
-	for rows.Next() {
+	result, err := h.Repository.GetAllActivities(r.Context())
 
-		var activity models.Activity
+	if err != nil {
+		panic(err)
+	}
 
-		if err := rows.Scan(
-			&activity.ID,
-			&activity.Title,
-			&activity.Start,
-			&activity.End,
-			&activity.Venue,
-			&activity.Host,
-			&activity.Status,
-			&activity.Remarks,
-		); err != nil {
-			utils.HandleHTTPError(w, "Failed reading columns", err)
-			return
-		}
-
+	// Convert each element in result to interface{} and append to activities
+	for _, activity := range result {
 		activities = append(activities, activity)
 	}
+
+	// const query = `SELECT id, title, start_date, end_date, venue, host, status, remarks FROM activities`
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer rows.Close()
+
+	// var activities []interface{}
+
+	// for rows.Next() {
+
+	// 	var activity models.Activity
+
+	// 	if err := rows.Scan(
+	// 		&activity.ID,
+	// 		&activity.Title,
+	// 		&activity.Start,
+	// 		&activity.End,
+	// 		&activity.Venue,
+	// 		&activity.Host,
+	// 		&activity.Status,
+	// 		&activity.Remarks,
+	// 	); err != nil {
+	// 		panic(err)
+	// 	}
+
+	// 	activities = append(activities, activity)
+	// }
 
 	data := &PageData{
 		Title:  "PTMS - Activities",
@@ -98,12 +71,131 @@ func (h *ActivityHandler) ListActivities(w http.ResponseWriter, r *http.Request)
 	utils.Render(w, "activities.html", data)
 }
 
+func (h *ActivityHandler) ShowActivity(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	activity, err := h.Repository.GetActivity(r.Context(), id)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// const query = `
+	// SELECT id, title, start_date, end_date, venue, host, status, remarks, created_at, updated_at
+	// FROM activities
+	// WHERE id = $1`
+
+	// id := r.PathValue("id")
+
+	// rows, err := h.DB.Query(r.Context(), query, id)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// defer rows.Close()
+
+	// var activity models.Activity
+
+	// for rows.Next() {
+
+	// 	if err := rows.Scan(
+	// 		&activity.ID,
+	// 		&activity.Title,
+	// 		&activity.Start,
+	// 		&activity.End,
+	// 		&activity.Venue,
+	// 		&activity.Host,
+	// 		&activity.Status,
+	// 		&activity.Remarks,
+	// 		&activity.CreatedAt,
+	// 		&activity.UpdatedAt,
+	// 	); err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+	fmt.Printf("activity: %v\n", activity)
+
+	data := &PageData{
+		Title:  "PTMS - Activities",
+		Icon:   "calendar-o",
+		Header: "Activities",
+		Data:   []interface{}{activity},
+	}
+
+	utils.Render(w, "activity.html", data)
+}
+
 func (h *ActivityHandler) ShowActivityForm(w http.ResponseWriter, r *http.Request) {
+	var payloads []interface{}
+
+	session := r.Context().Value(services.SessionKey{}).(*services.Session)
+	csrfToken, _ := h.Session.SetCSRFToken(session)
+
+	log.Println("csrfToken: " + csrfToken)
+
+	payloads = append(payloads, csrfToken)
+
 	data := &PageData{
 		Title:  "PTMS - New Activity",
 		Icon:   "calendar-o",
 		Header: "Activities",
+		Data:   payloads,
 	}
 
 	utils.Render(w, "create-activity.html", data)
+}
+
+func (h *ActivityHandler) CreateActivity(w http.ResponseWriter, r *http.Request) {
+	// const query = `
+	// INSERT INTO activities
+	// (title, start_date, end_date, venue, host, status,remarks)
+	// VALUES ($1,$2,$3,$4,$5,$6,$7)`
+
+	err := r.ParseForm()
+
+	if err != nil {
+		http.Error(w, "Error parsing form data", http.StatusBadRequest)
+		return
+	}
+
+	activityStatus, err := strconv.Atoi(r.FormValue("status"))
+
+	if err != nil {
+		http.Error(w, "Invalid status input", http.StatusBadRequest)
+		return
+	}
+
+	activity := &models.Activity{
+		Title:   r.FormValue("title"),
+		Start:   r.FormValue("start_date"),
+		End:     r.FormValue("end_date"),
+		Venue:   r.FormValue("venue"),
+		Host:    r.FormValue("host"),
+		Status:  activityStatus,
+		Remarks: r.FormValue("remarks"),
+	}
+
+	// title := r.FormValue("title")
+	// start := r.FormValue("start_date")
+	// end := r.FormValue("end_date")
+	// venue := r.FormValue("venue")
+	// host := r.FormValue("host")
+	// status := activityStatus
+	// remarks := r.FormValue("remarks")
+
+	_, err = h.Repository.CreateActivity(r.Context(), activity)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// defer rows.Close()
+
+	http.Redirect(w, r, "/activities", http.StatusSeeOther)
 }
--- internal/models/activity.go
+++ internal/models/activity.go
@@ -1,7 +1,16 @@
 package models
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/ferdiebergado/htmx-go/internal/db"
+)
+
 type Activity struct {
-	ID      int
+	Model
 	Title   string
 	Start   string
 	End     string
@@ -11,12 +20,120 @@ type Activity struct {
 	Remarks string
 }
 
-type Status map[int]string
+type ActivityRepository interface {
+	GetAllActivities(ctx context.Context) ([]*Activity, error)
+	GetActivity(ctx context.Context, id int) (*Activity, error)
+	CreateActivity(ctx context.Context, activity *Activity) (int, error)
+	UpdateActivity(ctx context.Context, activity *Activity) error
+	DeleteActivity(ctx context.Context, id int) error
+}
 
-var ActivityStatus = &Status{
+type activityRepository struct {
+	DB db.Database
+}
+
+var ActivityStatus = map[int]string{
 	1: "To be conducted",
 	2: "Conducted",
 	3: "Rescheduled",
 	4: "Postponed Indefinitely",
-	5: "Canceled",
+	5: "Cancelled",
+}
+
+func NewActivityRepository(db db.Database) *activityRepository {
+	return &activityRepository{DB: db}
+}
+
+func (r *activityRepository) GetAllActivities(ctx context.Context) ([]*Activity, error) {
+
+	const query = `
+	SELECT id, created_at, updated_at, title, start_date, end_date,  venue, host, status, remarks FROM activities
+	`
+	var activities []*Activity
+
+	rows, err := r.DB.Query(ctx, query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	err = db.MarshalRowsToStructs(rows, &activities)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return activities, nil
+}
+
+func (r *activityRepository) GetActivity(ctx context.Context, id int) (*Activity, error) {
+	const query = "SELECT id, created_at, updated_at, title, start_date, end_date, venue, host, status, remarks FROM activities WHERE id = $1"
+
+	var activity Activity
+
+	row := r.DB.QueryRow(ctx, query, id)
+
+	err := db.MarshalRowToStruct(row, &activity)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &activity, nil
+}
+
+func (r *activityRepository) CreateActivity(ctx context.Context, activity *Activity) (int, error) {
+	const query = `
+	INSERT INTO activities
+	(title, start_date, end_date, venue, host, status, remarks) 
+	VALUES ($1,$2,$3,$4,$5,$6,$7)
+	RETURNING id`
+
+	var id int
+
+	row := r.DB.QueryRow(ctx, query, activity.Title, activity.Start, activity.End, activity.Venue, activity.Host, activity.Status, activity.Remarks)
+
+	err := row.Scan(&id)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (r *activityRepository) UpdateActivity(ctx context.Context, activity *Activity) error {
+	const query = `
+	UPDATE activities 
+	SET title = $1, start_date = $2, end_date = $3, venue = $4, host = $5, status = $6, remarks = $7 
+	WHERE id = $8`
+
+	_, err := r.DB.Exec(ctx, query, activity.Title, activity.Start, activity.End, activity.Venue, activity.Host, activity.Status, activity.Remarks, activity.ID)
+
+	return err
+}
+
+func (r *activityRepository) DeleteActivity(ctx context.Context, id int) error {
+	const query = `DELETE FROM activities WHERE id = $1`
+
+	_, err := r.DB.Exec(ctx, query, id)
+
+	return err
+}
+
+func (a *Activity) ParseStatus(id int) string {
+	return fmt.Sprint(ActivityStatus[id])
+}
+
+func (a Activity) ParseDate(timeString string) string {
+
+	t, err := time.Parse(time.RFC3339, timeString)
+	if err != nil {
+		log.Println("Error parsing time:", err)
+		return timeString
+	}
+
+	return fmt.Sprint(t.Format(time.DateOnly))
 }
