ecdc44d5d4134f3e51b4500eed1be6c10553fff8
ferdie bergado <ferdiebergado@gmail.com>
2024-09-02 17:59:26 +0800

    Added session manager with csrf

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,10 @@
 package config
 
 const (
-	Port           string = "8888"
-	AssetsDir      string = "assets"
-	TemplatesDir   string = "templates"
-	MasterTemplate string = "layout.html"
+	Port            string = "8888"
+	AssetsDir       string = "assets"
+	TemplatesDir    string = "templates"
+	MasterTemplate  string = "layout.html"
+	SessionName     string = "sid"
+	SessionDuration int    = 30
 )
---
+++ internal/services/session.go
@@ -0,0 +1,185 @@
+package services
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/ferdiebergado/htmx-go/internal/config"
+)
+
+type Session struct {
+	ID        string
+	Data      map[string]interface{}
+	ExpiresAt time.Time
+}
+
+type SessionManager struct {
+	mu       sync.RWMutex
+	sessions map[string]*Session
+}
+
+type SessionKey struct{}
+
+func NewSessionManager() *SessionManager {
+	return &SessionManager{
+		sessions: make(map[string]*Session),
+	}
+}
+
+func (sm *SessionManager) CreateSession(w http.ResponseWriter) (*Session, error) {
+	log.Println("Creating new session...")
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	sessionID, err := generateSessionID()
+	if err != nil {
+		return nil, err
+	}
+
+	session := &Session{
+		ID:        sessionID,
+		Data:      make(map[string]interface{}),
+		ExpiresAt: time.Now().Add(time.Duration(config.SessionDuration) * time.Minute),
+	}
+
+	sm.sessions[sessionID] = session
+
+	http.SetCookie(w, &http.Cookie{
+		Name:     config.SessionName,
+		Value:    session.ID,
+		HttpOnly: true,
+		Secure:   true,
+		SameSite: http.SameSiteStrictMode,
+		Expires:  session.ExpiresAt,
+		Path:     "/",
+	})
+
+	return session, nil
+}
+
+func (sm *SessionManager) GetSession(r *http.Request) (*Session, error) {
+	log.Println("Retrieving cookie from request...")
+	sm.mu.RLock()
+	defer sm.mu.RUnlock()
+
+	cookie, err := r.Cookie(config.SessionName)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Cookie found. Reading session info...")
+
+	session, exists := sm.sessions[cookie.Value]
+
+	if !exists || session.ExpiresAt.Before(time.Now()) {
+		return nil, http.ErrNoCookie
+	}
+
+	log.Println("Cookie value: ", cookie.Value, " Session ID: ", session.ID)
+
+	return session, nil
+}
+
+func (sm *SessionManager) InvalidateSession(w http.ResponseWriter, r *http.Request) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+
+	cookie, err := r.Cookie(config.SessionName)
+	if err == nil {
+		delete(sm.sessions, cookie.Value)
+		http.SetCookie(w, &http.Cookie{
+			Name:     config.SessionName,
+			Value:    "",
+			MaxAge:   -1,
+			HttpOnly: true,
+			Secure:   true,
+			SameSite: http.SameSiteStrictMode,
+		})
+	}
+}
+
+func (sm *SessionManager) SetCSRFToken(session *Session) (string, error) {
+	token, err := generateCSRFToken()
+	if err != nil {
+		return "", err
+	}
+	log.Println("Setting csrf token...")
+	session.Data["csrf_token"] = token
+	return token, nil
+}
+
+func (sm *SessionManager) ValidateCSRFToken(session *Session, token string) bool {
+	log.Println("Validating csrf token...")
+	if storedToken, ok := session.Data["csrf_token"]; ok {
+		return storedToken == token
+	}
+	return false
+}
+
+func (sm *SessionManager) SessionMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session, err := sm.GetSession(r)
+		if err != nil {
+			session, err = sm.CreateSession(w)
+			if err != nil {
+				http.Error(w, "Unable to create session", http.StatusInternalServerError)
+				return
+			}
+		}
+
+		ctx := context.WithValue(r.Context(), SessionKey{}, session)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func (sm *SessionManager) CSRFMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		session := r.Context().Value(SessionKey{}).(*Session)
+
+		if r.Method == http.MethodPost {
+			csrfToken := r.FormValue("csrf_token")
+			if !sm.ValidateCSRFToken(session, csrfToken) {
+				http.Error(w, "Invalid CSRF token", http.StatusForbidden)
+				return
+			}
+
+			log.Println("csrf token valid.")
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func generateSessionID() (string, error) {
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(bytes), nil
+}
+
+func generateCSRFToken() (string, error) {
+	log.Println("Generating csrf token...")
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+
+	token := base64.URLEncoding.EncodeToString(bytes)
+
+	log.Println("Csrf token: ", token)
+
+	return token, nil
+}
+
+func GetCsrf(r *http.Request, s *SessionManager) string {
+	session := r.Context().Value(SessionKey{}).(*Session)
+	csrfToken, _ := s.SetCSRFToken(session)
+
+	return csrfToken
+}
