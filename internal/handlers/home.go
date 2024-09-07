d99859d02d543153bfbff6e554456b29a0c89174
ferdie bergado <ferdiebergado@gmail.com>
2024-09-07 21:23:47 +0800

    Fixed error handling

--- internal/handlers/home.go
+++ internal/handlers/home.go
@@ -10,8 +10,9 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	// The "/" pattern matches everything, so we need to check
 	// that we're at the root here.
 	if r.URL.Path != "/" {
+		// http.Error(w, "page not found", http.StatusNotFound)
 		w.WriteHeader(http.StatusNotFound)
-		utils.Render(w, r, "notfound.html", nil)
+		// utils.Render(w, r, "notfound.html", nil)
 		return
 	}
 
