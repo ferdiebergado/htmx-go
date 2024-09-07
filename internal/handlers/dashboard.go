2155b003c2d28fd36735e834b56b792c1796ed6e
ferdie bergado <ferdiebergado@gmail.com>
2024-09-07 21:22:58 +0800

    Removed root path check

--- internal/handlers/dashboard.go
+++ internal/handlers/dashboard.go
@@ -8,12 +8,6 @@ import (
 
 func ShowDashboard(w http.ResponseWriter, r *http.Request) {
 
-	if r.URL.Path != "/" {
-		w.WriteHeader(http.StatusNotFound)
-		utils.Render(w, r, "notfound.html", nil)
-		return
-	}
-
 	data := &PageData{
 		Title:  "Personnel Travel Monitoring System - Dashboard",
 		Icon:   "dashboard",
